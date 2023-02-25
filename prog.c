ce547454cf8470e6f2c11fbcc772bb50cd1ebd10
bagoferasers <ctrull60@gmail.com>
2023-02-25 13:28:30 -0600

    opening file
    yes I am baby

--- prog.c
+++ prog.c
@@ -10,9 +10,22 @@ int Total_waiting_time = 0;
 int Total_turnaround_time = 0;
 int Total_job = 0;
 
-int main( )
+int main( int argc, char* argv[ ] )
 {
+    printf( "Student Name: Colby Bailey ( nib667 )\n" );
     
+    FILE* f = NULL;
+    if( argv[ 1 ] == NULL || !fopen( argv[ 1 ], "r+" ) )
+    {
+        fprintf( stderr, "ERROR: file does not exist or cannot be opened. Oops!\n" );
+        return 1;
+    }
+    else
+    {
+        f = fopen( argv[ 1 ], "r+" );
+        printf( "Input File Name: %s\n", argv[ 3 ] );
+    }
 
+    fclose( f );
     return 0;
 }
