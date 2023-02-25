0f00d8d9f39d97825b0990773502f9a120bc4e3a
bagoferasers <ctrull60@gmail.com>
2023-02-25 15:03:30 -0600

    I love TAIL
    oh yaaaaaaaa

--- prog.c
+++ prog.c
@@ -9,7 +9,7 @@ int CLOCK = 0;
 int Total_waiting_time = 0;
 int Total_turnaround_time = 0;
 int Total_job = 0;
-int id, pr, burst;
+int id, priority, burst;
 
 int main( int argc, char* argv[ ] )
 {
@@ -34,7 +34,7 @@ int main( int argc, char* argv[ ] )
 
     // scan lines
     struct PCB_st* p;
-    while( ( fscanf( f, "%d %d %d", &id, &pr, &burst ) ) != EOF )
+    while( ( fscanf( f, "%d %d %d", &id, &priority, &burst ) ) != EOF )
     {
         p = ( struct PCB_st* )malloc( sizeof( struct PCB_st ) );
         if( !p )
@@ -42,6 +42,23 @@ int main( int argc, char* argv[ ] )
             free( p );
             fprintf( stderr, "ERROR: could not allocate memory for struct.\n" );
             return -1;
+        }
+        p->ProcId = id;
+        p->ProcPR = priority;
+        p->CPUburst = burst;
+        for( int k = 0; k < 8; k++ )
+            p->myReg[ k ] = id;
+        p->queueEnterClock = 0;
+        p->waitingTime = 0;
+        if( Head == NULL )
+        {
+            Head = p;
+            Tail = p;
+        }
+        else
+        {
+            Tail->next = p;
+            Tail = p;
         }
     }
 
